240a7a4d00f2324c4aa6d9e866c0d98909e8603c
Liubov Pellinen <carie@mail.ru>
2021-04-11 17:31:16 +0300

    modification

--- models/reminder.js
+++ models/reminder.js
@@ -3,7 +3,6 @@ if ( process.env.NODE_ENV !== 'production' ) {
     require('dotenv').config()
   }
 const url = process.env.MONGODB_URI
-//const url = 'mongodb+srv://Liubov:Reminders1004@clusterliubov.mfzvr.mongodb.net/reminders_utu?retryWrites=true&w=majority'
 mongoose.connect(url,{useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
 const Reminder = mongoose.model('Reminder', {
   name: String,
